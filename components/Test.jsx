561faaf0bfa66f202dbae38ed6645f4efb1e34fb
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2023-04-10 18:49:49 +0530

    pushing the code on github to deploy

---
+++ components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+    useEffect(() => {
+    console.log('shivam from test');
+    }, [])
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    PROJECTS_PATH: '/projects'
+  }
 }
 
 module.exports = nextConfig
